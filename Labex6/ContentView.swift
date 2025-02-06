eb62d2879fa1d2bf92a4ce34942f7d9e9714e2d2
lapointek <156107790+lapointek@users.noreply.github.com>
2025-02-06 18:44:15 -0500

    added buttons and output

--- Labex6/ContentView.swift
+++ Labex6/ContentView.swift
@@ -8,16 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var outputValue: Int = 0
+    @State private var stepValue: Int = 1
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+            VStack {
+                Image("Calculator")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height:100)
+                    .padding()
+                
+                Text("\(outputValue)")
+                    .font(.largeTitle)
+                    .padding()
+                
+                HStack{
+                    Button("Reset", action: {
+                        outputValue = 0
+                    }).buttonStyle(.bordered)
+                    Button("Step", action: {
+                        stepValue = (stepValue == 1) ? 2 : 1
+                    }).buttonStyle(.bordered)
+                    Button("Subtract", action: {
+                        outputValue -= stepValue
+                    }).buttonStyle(.bordered)
+                    Button("Add", action: {
+                        outputValue += stepValue
+                    }).buttonStyle(.bordered)
+            }
         }
-        .padding()
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 #Preview {
     ContentView()
