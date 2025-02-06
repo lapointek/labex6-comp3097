8e455e2d2d7e93def474fcfcf13bd40af2c10de9
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-06 15:18:05 -0500

    Initial Commit

---
+++ Labex6/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Labex6
+//
+//  Created by Kevin Lapointe on 2025-02-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Labex6/Labex6App.swift
@@ -0,0 +1,17 @@
+//
+//  Labex6App.swift
+//  Labex6
+//
+//  Created by Kevin Lapointe on 2025-02-06.
+//
+
+import SwiftUI
+
+@main
+struct Labex6App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
